c97565e26a63e3bed6c52090959f023ee158951f
Simon Marin Giraldo <smaring1@eafit.edu.co>
2021-03-22 12:45:05 -0500

    added the palindrome linked list problem

---
+++ Problems/Problems/Leetcode/Python/isPalindrome.py
@@ -0,0 +1,13 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        lis = []
+        cur = head
+        while cur:
+            lis.append(cur.val)
+            cur = cur.next
+        return lis == lis[::-1]
