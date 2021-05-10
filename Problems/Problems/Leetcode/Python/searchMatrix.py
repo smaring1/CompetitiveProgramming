a766ccc5d7a9f502b7289ae43524c62efbfddcaf
Simon Marin Giraldo <smaring1@eafit.edu.co>
2021-05-09 22:15:37 -0500

    added the searchMatrix problem (problem with binary search in arrays)

---
+++ Problems/Problems/Leetcode/Python/searchMatrix.py
@@ -0,0 +1,33 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        def binarySearch(arr, l, r, x):
+            # Check base case
+            if r >= l:
+                mid = l + (r - l) // 2
+
+                # If element is present at the middle itself
+                if arr[mid] == x:
+                    return mid
+
+                # If element is smaller than mid, then it
+                # can only be present in left subarray
+                elif arr[mid] > x:
+                    return binarySearch(arr, l, mid-1, x)
+
+                # Else the element can only be present
+                # in right subarray
+                else:
+                    return binarySearch(arr, mid + 1, r, x)
+
+            else:
+                # Element is not present in the array
+                return -1
+        for i in range(len(matrix)):
+            if not target > matrix[i][-1]:
+                a = binarySearch(matrix[i], 0, len(matrix[i])-1, target)
+                if a != -1:
+                    return True
+                else:
+                    return False
+        return False
+        
