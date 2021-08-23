993e5503d1dbdfcc5b3e440fea57efe9dd60ef6f
Simon Marin Giraldo <smaring1@eafit.edu.co>
2021-08-23 16:51:27 -0500

    added the build array from permutation problem

--- Problems/Problems/Leetcode/Python/pruebaLista.py
+++ Problems/Problems/Leetcode/Python/pruebaLista.py
@@ -5,22 +5,46 @@ class ListNode:
         self.next = next
 
 
-def maximumList(head):
-    if head:
-        maximum = head.val
-        while head:
-            if head.val > maximum:
-                maximum = head.val
-            head = head.next
-        return maximum
-    else:
-        return 0
+def addTwoNumbers(l1, l2):
+    dummy = ListNode()
+    cur = dummy
+    carry = 0
+    while l1 or l2:
+        if l1:
+            x = l1.val
+        else:
+            x = 0
+        if l2:
+            y = l2.val
+        else:
+            y = 0
+        suma = carry + x + y
+        carry = suma // 10
+        cur.next = ListNode(val=suma%10)
+        cur = cur.next
+        if l1:
+            l1 = l1.next
+        if l2:
+            l2 = l2.next
+    if carry > 0:
+        cur.next = ListNode(val=carry)
+    return dummy.next
 
 l1 = ListNode()
-l1.val = 1
+l1.val = 2
 l1.next = ListNode()
-l1.next.val = 2
+l1.next.val = 4
 l1.next.next = ListNode()
 l1.next.next.val = 3
 
-print(maximumList(l1))
+l2 = ListNode()
+l2.val = 5
+l2.next = ListNode()
+l2.next.val = 6
+l2.next.next = ListNode()
+l2.next.next.val = 4
+
+result = addTwoNumbers(l1, l2)
+while result:
+    print(result.val)
+    result = result.next
---
+++ Problems/Problems/Leetcode/Python/sumaNumeros.py
@@ -0,0 +1,6 @@
+entrada = int(input("Ingese un numero: "))
+def sumaNumeros(nums, total):
+    if len(nums) == 0:
+        return total
+
+print(sumaNumeros(entrada, total))
