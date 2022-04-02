c0efd23a47947b8b49a62154ff9d592c489880b0
Simón Marín Giraldo <smaring1@eafit.edu.co>
2022-04-01 22:25:58 -0500

    Added the 13. Roman to Integer problem

--- Problems/Problems/Leetcode/Python/romans.py
+++ Problems/Problems/Leetcode/Python/romans.py
@@ -1,13 +1,16 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        total = 0
-        i = 0
-        while i < len(s):
-            if i+1 < len(s) and values[s[i]] < values[s[i+1]]:
-                total += values[s[i+1]] - values[s[i]]
-                i += 1
+        values = {'I': 1,
+                  'V': 5,
+                  'X': 10,
+                  'L': 50,
+                  'C': 100,
+                  'D': 500,
+                  'M': 1000}
+        cont = 0
+        for i in range(len(s)):
+            if i < len(s)-1 and values[s[i]] < values[s[i+1]]:
+                cont -= values[s[i]]
             else:
-                total += values[s[i]]
-            i += 1
-        return total
+                cont += values[s[i]]
+        return cont
